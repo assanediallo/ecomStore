203f445245182cf0c2286d8079c0edc1025e03a5
Assane DIALLO <assane.diallo0612@gmail.com>
2022-07-20 09:46:49 +0200

    tests auto deployment

--- src/shop/Banner.js
+++ src/shop/Banner.js
@@ -3,6 +3,7 @@ import logo from '../assets/logo.webp'
 function Banner() {
   return <div className="banner">
     <img src={logo} className="logo" />
+    <h1>Deploiement automatic</h1>
   </div>
 }
 export default Banner
